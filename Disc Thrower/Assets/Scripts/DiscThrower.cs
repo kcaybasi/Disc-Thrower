835f32b7790b8cf9249705109871605a57afa3d5
Kadir Furkan Çaybaşı <k.furkancaybas@gmail.com>
2022-11-21 17:36:29 +0300

    1.0.4-Disc thrower started.

--- Disc Thrower/Assets/Scripts/DiscThrower.cs
+++ Disc Thrower/Assets/Scripts/DiscThrower.cs
@@ -1,9 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DiscThrower : MonoBehaviour
 {
-    
+    private bool _isThrowAllowed=true;
+    void ThrowDisc()
+    {
+        GameObject obj = ObjectPooler.Instance.DiscPool.Get();
+        obj.transform.position = transform.position;
+        float currentZ = obj.transform.position.z;
+        obj.transform.DOMoveZ(currentZ + 50f, 5f);
 
+
+    }
+
+    private void Update()
+    {
+        if (_isThrowAllowed)
+        {
+            StartCoroutine(DiscThrowRoutine());
+        }
+    }
+
+    IEnumerator DiscThrowRoutine()
+    {
+        _isThrowAllowed = false;
+        ThrowDisc();
+        yield return new WaitForSeconds(1.5f);
+        
+        _isThrowAllowed = true;
+    }
 }

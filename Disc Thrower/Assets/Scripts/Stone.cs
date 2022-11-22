ec7e352863de86c5c4214f2c6cb6ccf45e5f3c65
Kadir Furkan Çaybaşı <k.furkancaybas@gmail.com>
2022-11-22 16:53:50 +0300

    1.0.16-Tower destroy effect added.

--- Disc Thrower/Assets/Scripts/Stone.cs
+++ Disc Thrower/Assets/Scripts/Stone.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 using DG.Tweening;
 
 public class Stone : MonoBehaviour
-{
+{ 
+    private ParticleSystem _gibletParticle;
+
+    private void Awake()
+    {
+        _gibletParticle = transform.parent.GetChild(0).GetComponent<ParticleSystem>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Disc"))
         {
-            transform.DOScale(Vector3.zero, .2f);
+            
+            _gibletParticle.Play();
+            transform.DOScale(Vector3.zero, .25f);
             other.GetComponent<Collider>().enabled = false;
         }
     }
